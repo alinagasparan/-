5112da04ff65d7a4f838908353e4ccb0a74cf699
alinagasparan <alinagasparan0908@gmail.com>
2026-04-07 10:07:43 +0000

    mnist.npz

--- prog.py
+++ prog.py
@@ -1,22 +1,21 @@
 
 import numpy as np
-import urllib.request
-base_url = "https://storage.googleapis.com/cvdf-datasets/mnist/"
 
-X = mnist.data.values
-y = mnist.target.astype(int).values
+data = np.load("mnist.npz")
 
-# разделение
-x_train, x_test = X[:60000], X[60000:]
-y_train, y_test = y[:60000], y[60000:]
+x_train = data['x_train']
+y_train = data['y_train']
+x_test = data['x_test']
+y_test = data['y_test']
+
+x_train = x_train.reshape(x_train.shape[0], -1)
+x_test = x_test.reshape(x_test.shape[0], -1)
 
 class Linear:
 
     def fit(self, X, y):
 
-
         X = np.insert(X, 0, 1, axis=1)
-
         XtX_inv = np.linalg.pinv(X.T @ X)
         weights = XtX_inv @ X.T @ y
 
@@ -30,13 +29,6 @@ class Linear:
         preds = self.predict(X)
         preds = np.round(preds)
         return np.mean(preds == y)
-
-
-
-(x_train, y_train), (x_test, y_test) = mnist.load_data()
-
-x_train = x_train.reshape(x_train.shape[0], -1)
-x_test = x_test.reshape(x_test.shape[0], -1)
 
 model = Linear()
 model.fit(x_train, y_train)
